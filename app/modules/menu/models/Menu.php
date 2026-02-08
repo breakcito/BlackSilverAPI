bde43f2378569667b80977adf62b60134d30d228
Break <franklinbacacampos@gmail.com>
2026-02-08 18:46:05 -0500

    modelo de Menu que se encargara de devolver los registros que ayudaran a construir el menu de navegacion por usuario

--- app/modules/menu/models/Menu.php
+++ app/modules/menu/models/Menu.php
@@ -11,25 +11,86 @@ use Illuminate\Support\Facades\DB;
  */
 class Menu extends Model
 {
-    public static function get_secciones_by_rol(int $id_rol): array
+    public static function get_modulos_by_rol(int $id_rol)
     {
         $sql = '
-            SELECT
-                m.id,
-                m.nombre,
-                m.descripcion,
-                m.estado,
-                COUNT(s.id) as total_submodulos
-            FROM modulo m
-            LEFT JOIN submodulo s ON s.id_modulo = m.id AND s.estado = ?
-            WHERE m.estado = ?
-            GROUP BY m.id, m.nombre, m.descripcion, m.estado
-            ORDER BY m.nombre
+        /*
+        Obtener los modulos para el menu
+        de navegacion
+        */
+        SELECT DISTINCT
+            md.id AS id_modulo,
+            md.nombre
+        FROM
+            modulo md
+        INNER JOIN submodulo sb ON
+            sb.id_modulo = md.id
+        INNER JOIN seccion sc ON
+            sc.id_submodulo = sb.id
+        INNER JOIN seccion_rol scr ON
+            scr.id_seccion = sc.id
+        WHERE
+            scr.id_rol = ?
+        ORDER BY md.nombre;
         ';
 
         return DB::select($sql, [
-            EstadoBase::Activo->value,
-            EstadoBase::Activo->value,
+            $id_rol,
+        ]);
+    }
+
+    public static function get_submodulos_by_rol_and_modulo(int $id_rol, int $id_modulo): array
+    {
+        $sql = '
+        /*
+        Obtener los submodulos para el menu
+        de navegacion
+        */
+        SELECT DISTINCT
+            sb.id as id_submodulo,
+            sb.nombre
+        FROM
+            submodulo sb
+        INNER JOIN seccion sc ON
+            sc.id_submodulo = sb.id
+        INNER JOIN seccion_rol scr ON
+            scr.id_seccion = sc.id
+        WHERE
+            scr.id_rol = ? AND 
+            sb.id_modulo = ?
+        ORDER BY sb.nombre;
+        ';
+
+        return DB::select($sql, [
+            $id_rol,
+            $id_modulo,
+        ]);
+    }
+
+    public static function get_secciones_by_rol_and_submodulo(int $id_rol, int $id_submodulo): array
+    {
+        $sql = '
+        /*
+        Obtener las secciones para el menu
+        de navegacion
+        */
+        SELECT DISTINCT
+            sc.id AS id_seccion,
+            sc.nombre,
+            sc.url
+        FROM
+            seccion sc
+        INNER JOIN seccion_rol scr ON
+            scr.id_seccion = sc.id
+        WHERE
+            scr.id_rol = ? AND sc.id_submodulo = ?
+        ORDER BY
+            sc.nombre;
+        ';
+
+        return DB::select($sql, [
+            $id_rol,
+            $id_submodulo,
         ]);
     }
 }
