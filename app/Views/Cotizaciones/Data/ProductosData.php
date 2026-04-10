adad9a8f7fbcb772e63dd798c80597b2962065ab
Boyita Boy <anabelensanchezboy@gmail.com>
2026-04-10 13:25:14 -0500

    se insertó categoria

--- app/Views/Cotizaciones/Data/ProductosData.php
+++ app/Views/Cotizaciones/Data/ProductosData.php
@@ -17,10 +17,12 @@ class ProductosData
             SELECT 
                 p.id AS id_producto,
                 p.nombre,
+                c.nombre AS categoria_nombre,
                 p.id_unidad_medida_base,
                 um.nombre AS unidad_medida_base,
                 um.abreviatura AS unidad_medida_abreviatura
             FROM producto p
+            INNER JOIN categoria c ON c.id = p.id_categoria
             INNER JOIN unidad_medida um ON um.id = p.id_unidad_medida_base
             WHERE p.estado = ?
             ORDER BY p.nombre ASC
