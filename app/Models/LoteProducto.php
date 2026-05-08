fa51c4f108872de099df9961b1d69be67c9d52f6
Break <franklinbacacampos@gmail.com>
2026-05-08 11:39:40 -0500

    nuevos campos en la tabla del lote para conocer su origen y su costo por unidad base, unidad del lote y el subtotal inicial

--- app/Models/LoteProducto.php
+++ app/Models/LoteProducto.php
@@ -12,17 +12,27 @@ class LoteProducto extends Model
 
     protected $fillable = [
         'id_producto',
-        'id_unidad_medida',
-        'id_almacen',
-        'descripcion',
-        'correlativo',
+        'id_unidad_medida', // unidad de medida del lote
+        'id_almacen', // a que almacen le pertenece este lote
+        'id_origen', // el id del registro de donde provino el lote
+        //
+        'tabla_origen', // el nombre de la tabla de donde provino el lote
+        //
+        'correlativo', // LOT-
         'numero_correlativo',
-        'stock_actual',
-        'contenido_por_presentacion',
-        'costo_base', // el costo en base a la unidad base
-        'stock_actual_base',
+        'descripcion',
+        //
+        'stock_actual', // segun la unidad del lote
+        'contenido_por_presentacion', // cuantas unidades del producto hay en una unidad del lote: Ej. 12KG x Saco
+        'stock_actual_base', // segun la unidad base del producto
+        //
+        'costo_por_unidad', // el costo en base a la unidad del lote
+        'costo_por_unidad_base', // el costo en base a la unidad del producto
+        'subtotal_inicial', // es el costo inicial del lote, pues su valor actual dependera del stock actual que tenga
+        //
         'fecha_hora_ingreso',
         'fecha_vencimiento',
+        //
         'created_at',
         'estado',
     ];
