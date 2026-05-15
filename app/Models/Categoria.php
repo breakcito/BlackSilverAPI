9894d2c35b1206e072caf3bce2912a88b27a95f3
Break <franklinbacacampos@gmail.com>
2026-05-14 23:29:22 -0500

    registrar categorias de activos fijos precisando el control que desean tener

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -14,11 +14,21 @@ class Categoria extends Model
         'nombre',
         'descripcion',
         'tipo_producto', // Bien o Servicio
-        'clasificacion_bien', // Suministro, Materiales, Activo Fijo
+        'clasificacion_bien', // Suministro, Material, Activo Fijo
         'es_consumible', // si es true, significa que se podra indicar qué otras categorias consumen esta categoria
         'es_auditable', // bool que ayuda a saber si los productos de esa categoria con auditables para ocultarlos
-        'para_cocina',
-        'para_mina',
-        'estado',
+        //
+        // Si es activo fijo
+        // al registar un activo fijo de de esta categoria sera necesario ingresar datos propios de un vehiculo - para 
+        // consultas y saber los vehiculos que tiene el consorcio
+        'para_transporte', 
+        'control_por_odometro', // util para saber si llevara un control por kilometraje recorrido - para el modulo de Uso
+        'control_por_horometro', // util para saber si llevara un control por horas de trabajo - para el modulo de Uso
+        //
+        // Destinos de uso
+        'para_cocina', // indica que los productos de esta categoria seran para cocina
+        'para_mina', // indica que los productos de esta categoria seran para mina
+        // 
+        'estado', // Estado Base
     ];
 }
--- app/Shared/Enums/_Generic/TipoBien.php
+++ app/Shared/Enums/_Generic/TipoBien.php
@@ -5,6 +5,6 @@ namespace App\Shared\Enums\_Generic;
 enum TipoBien: string
 {
     case Suministro = 'Suministro';
-    case Materiales = 'Materiales';
+    case Material = 'Material';
     case ActivoFijo = 'Activo Fijo';
 }
